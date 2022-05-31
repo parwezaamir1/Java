55faeb8ab7c98c867d59c8f41ef6aace281b3c93
parwezaamir1 <parwez.aamir777@gmail.com>
2022-05-31 20:45:06 +0530

    31st May Constructors

---
+++ Java Masterclass/Inheritance/src/com/company/Dog.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class Dog extends Animal {
+
+    private int eyes;
+    private int legs;
+    private int tail;
+    private int teeth;
+    private String coat;
+
+    public Dog(String name, int size, int weight, int eyes, int legs, int tail, int teeth, String coat) {
+        super(name, 1, 1, size, weight);
+        this.eyes = eyes;
+        this.legs = legs;
+        this.tail = tail;
+        this.teeth = teeth;
+        this.coat = coat;
+    }
+
+    public void chew(){
+        System.out.println("Dog.chew() is called");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Dog.eat() is called");
+        chew();
+        super.eat();
+    }
+
+    public void walk(){
+        System.out.println("Dog.walk() called");
+        move(5);
+    }
+
+    public void run(){
+        System.out.println("Dog.run() callled");
+        move(10);
+    }
+}
