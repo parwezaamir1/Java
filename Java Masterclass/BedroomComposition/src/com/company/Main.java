5dda932a81cb50f5026c603b052f6a9a965f16aa
parwezaamir1 <parwez.aamir777@gmail.com>
2022-06-14 17:01:26 +0530

    compositions

---
+++ Java Masterclass/BedroomComposition/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        Wall wall1= new Wall("west");
+        Wall wall2 = new Wall("east");
+        Wall wall3 = new Wall("south");
+        Wall wall4 = new Wall("north");
+
+        Ceiling ceiling = new Ceiling(12, 55);
+        Bed bed = new Bed("Modern", 4,3,2,1);
+        Lamp lamp = new Lamp("Classic", false,75);
+
+        Bedroom bedroom = new Bedroom("Name",wall1,wall2,wall3,wall4,ceiling,bed,lamp);
+        bedroom.makeBed();
+        bedroom.getLamp().turnOn();
+    }
+
+}
